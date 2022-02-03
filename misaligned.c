bdec40bfb7f4d785d0c1eb3307a63753a3d28bf9
SaraGupta1410 <91050951+SaraGupta1410@users.noreply.github.com>
2022-02-03 23:45:25 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,40 +1,60 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
-#define Max_Major_Color 5U
-#define Max_Minor_Color 5U
+#define COLOR_PAIR_COUNT  25
 
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-/* This function will be check pair names aginst pair number befor getting print */
-
-void printOnConsole(int AIndex_i , const char* AmajorColor_c , const char* AminorColor_c)
+struct ColorPair
 {
-    /* Below assert function will be check each pair */
-    
-    assert( majorColor[AIndex_i / Max_Major_Color] == AmajorColor_c);
-    assert( minorColor[AIndex_i % Max_Minor_Color] == AminorColor_c);
-     
-    printf("%d | %s | %s\n", AIndex_i , AmajorColor_c, AminorColor_c);
+    int  pairNumber;
+    char MajorColorName[15];
+    char MinorColorName[15];
+};
+
+struct ColorPair colorPairInfo[COLOR_PAIR_COUNT];
+
+void printToConsole(struct ColorPair* colorPairPtr, int pairIndex)
+{
+	printf("%d | %s | %s\n", colorPairPtr[pairIndex].pairNumber, colorPairPtr[pairIndex].MajorColorName, colorPairPtr[pairIndex].MinorColorName);
 }
 
+int printColorMap(struct ColorPair* colorPairInfoPtr) {
 
-int printColorMap() {
-    int i = 0, j = 0;
+    int i = 0, j = 0, pairNumberIndex = 0;
     for(i = 0; i < 5; i++) {
-       for(j = 0; j < 5; j++) 
-       {
-           printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
-           printOnConsole(i * 5 + j , majorColor[i] ,  minorColor[i]);
+        for(j = 0; j < 5; j++) {
+        	colorPairInfoPtr[pairNumberIndex].pairNumber = (i * 5 + j + 1);
+        	strcpy(colorPairInfoPtr[pairNumberIndex].MajorColorName , majorColor[i]);
+        	strcpy(colorPairInfoPtr[pairNumberIndex].MinorColorName , minorColor[j]);
+            printToConsole(colorPairInfoPtr, pairNumberIndex);
+
+            pairNumberIndex++;
         }
     }
     return i * j;
 }
 
+void testColorMap(struct ColorPair* testcolorPairInfoPtr)
+{
+	int i = 0, j = 0, pairNumberIndex = 0;
+	for(i = 0; i < 5; i++) {
+	    for(j = 0; j < 5; j++) {
+	        assert(testcolorPairInfoPtr[pairNumberIndex].pairNumber == (i * 5 + j + 1));
+	        assert(!(strcmp(testcolorPairInfoPtr[pairNumberIndex].MajorColorName , majorColor[i])));
+	        assert(!(strcmp(testcolorPairInfoPtr[pairNumberIndex].MinorColorName , minorColor[j])));
+
+	        pairNumberIndex++;
+	     }
+         }
+}
+
 int main() {
-    int result = printColorMap();
+    int result = printColorMap(colorPairInfo);
     assert(result == 25);
+    testColorMap(colorPairInfo);
     printf("All is well (maybe!)\n");
     return 0;
 }
